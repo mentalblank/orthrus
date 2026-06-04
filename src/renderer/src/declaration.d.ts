1f06e0bc140811b4f6a924cc0b50edc3838d1cbc
Zamitto <167933696+zamitto@users.noreply.github.com>
2026-06-04 10:45:10 -0300

    chore: update to electron 40 (#2208)
    * chore: update to electron 40
    
    * feat: remove unused prop for extraction on leveldb and simplify calls
    
    ---------
    
    Co-authored-by: Chubby Granny Chaser <weak.fern2638@heliokroger.com>

--- src/renderer/src/declaration.d.ts
+++ src/renderer/src/declaration.d.ts
@@ -357,6 +357,11 @@ declare global {
       cb: () => void
     ) => () => Electron.IpcRenderer;
 
+    /* Clipboard */
+    clipboard: {
+      writeText: (text: string) => Promise<void>;
+    };
+
     /* Misc */
     openExternal: (src: string) => Promise<void>;
     getVersion: () => Promise<string>;
